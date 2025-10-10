ee2a8189b23ff564a28def414ef82542b6dd6d19
LUVTTU <robomancc@gmail.com>
2025-10-10 19:50:30 +1300

    update

--- Assignment1/wsgi.py
+++ Assignment1/wsgi.py
@@ -9,7 +9,6 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 
 import os
 from django.core.wsgi import get_wsgi_application
-from whitenoise import WhiteNoise
 from pathlib import Path
 
 # Use vercel settings in production
@@ -20,10 +19,8 @@ else:
 
 application = get_wsgi_application()
 
-# Add WhiteNoise for static files
-application = WhiteNoise(application, root=Path(__file__).parent.parent / 'staticfiles')
-application.add_files(Path(__file__).parent.parent / 'media', prefix='media/')
-
-
-application = get_wsgi_application()
-app = application
+# Only use WhiteNoise if not on Vercel (Vercel handles static files differently)
+if not os.environ.get('VERCEL') == '1':
+    from whitenoise import WhiteNoise
+    application = WhiteNoise(application, root=Path(__file__).parent.parent / 'staticfiles')
+    application.add_files(Path(__file__).parent.parent / 'media', prefix='media/')
