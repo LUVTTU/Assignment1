7553caceec87aa9e243f6db23b5602bc6909b85b
LUVTTU <robomancc@gmail.com>
2025-10-10 13:17:50 +1300

    fix?

--- Assignment1/wsgi.py
+++ Assignment1/wsgi.py
@@ -8,11 +8,21 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
+from whitenoise import WhiteNoise
+from pathlib import Path
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Assignment1.settings')
+# Use vercel settings in production
+if os.environ.get('VERCEL') == '1':
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Assignment1.vercel_settings')
+else:
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Assignment1.settings')
 
 application = get_wsgi_application()
 
+# Add WhiteNoise for static files
+application = WhiteNoise(application, root=Path(__file__).parent.parent / 'staticfiles')
+application.add_files(Path(__file__).parent.parent / 'media', prefix='media/')
+
+# Vercel needs a variable named 'app' for serverless functions
 app = application
